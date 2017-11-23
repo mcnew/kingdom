514ad84ad6fb0398124a1dd526eb273bc186a52c
razzek <razzek@i7-node>
2017-11-23 12:35:28 -0600

    saving state

--- src/main/java/mx/dannyyesoft/crm/CrmApplication.java
+++ src/main/java/mx/dannyyesoft/crm/CrmApplication.java
@@ -1,12 +1,40 @@
 package mx.dannyyesoft.crm;
 
+import static springfox.documentation.builders.PathSelectors.regex;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
+@ComponentScan("mx.dannyyesoft.crm.controller")
 public class CrmApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrmApplication.class, args);
 	}
+
+	@Bean
+	public Docket newsApi() {
+		return new Docket(DocumentationType.SWAGGER_2).groupName("kingdom").apiInfo(apiInfo()).select()
+				.paths(regex("/kingdom.*")).build();
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder().title("Spring REST Sample with Swagger")
+				.description("Spring REST Sample with Swagger")
+				// .termsOfServiceUrl("http://www-03.ibm.com/software/sla/sladb.nsf/sla/bm?Open")
+				.contact("www.dannyyesoft.mx").license("Todos los derechos reservados")
+				// .licenseUrl("https://github.com/IBM-Bluemix/news-aggregator/blob/master/LICENSE").version("1.0")
+				.build();
+	}
+
 }
--- src/main/java/mx/dannyyesoft/crm/controller/KingdomController.java
+++ src/main/java/mx/dannyyesoft/crm/controller/KingdomController.java
@@ -2,12 +2,15 @@ package mx.dannyyesoft.crm.controller;
 
 import java.net.URI;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -16,24 +19,29 @@ import mx.dannyyesoft.crm.entity.KingdomEntity;
 @RestController
 public class KingdomController {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(KingdomController.class);
+
 	@RequestMapping(path = "/kingdom", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	public ResponseEntity<KingdomEntity> get() {
+	@ResponseBody
+	public ResponseEntity<KingdomEntity> list() {
 		return ResponseEntity.ok().build();
 	}
 
 	@RequestMapping(path = "/kingdom", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	public ResponseEntity<Void> post(@RequestBody KingdomEntity kingdom, UriComponentsBuilder ucBuilder) {
+	public ResponseEntity<Void> create(@RequestBody KingdomEntity kingdom, UriComponentsBuilder ucBuilder) {
 		URI location = ucBuilder.path("/kingdom/{id}").buildAndExpand(kingdom.getId()).toUri();
 		return ResponseEntity.created(location).build();
 	}
 
 	@RequestMapping(path = "/kingdom", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	public ResponseEntity<Void> put(@RequestBody KingdomEntity kingdom) {
+	public ResponseEntity<Void> update(@RequestBody KingdomEntity kingdom) {
 		return ResponseEntity.ok().build();
 	}
 
-	@RequestMapping(path = "/kingdom/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	public ResponseEntity<KingdomEntity> detail(@PathVariable Integer kingdomId) {
+	@RequestMapping(value = "/kingdom/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	@ResponseBody
+	public ResponseEntity<KingdomEntity> read(@PathVariable("id") Integer kingdomId) {
+		LOGGER.trace("{}", kingdomId);
 		return ResponseEntity.ok().build();
 	}
 
